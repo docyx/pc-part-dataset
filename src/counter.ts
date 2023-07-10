bc4591bdb94f75d7a317bc96df1e1e0c47f05f8f
docyx <oliverxur@gmail.com>
2023-07-10 18:27:31 -0400

    [counter] take in dirname as cli argument
    defaults to `data/json`. this also skips files that aren't `.json`
    (looking at you .DS_Store)

--- src/counter.ts
+++ src/counter.ts
@@ -1,12 +1,15 @@
 import { readdir, readFile } from 'fs/promises'
 import { join } from 'path'
 ;(async () => {
-	const files = await readdir('data-staging')
+	const dirName = process.argv.slice(2)[0] ?? 'data/json'
+	const files = await readdir(dirName)
 
 	let count = 0
 
 	for (const file of files) {
-		const raw = await readFile(join('data-staging', file))
+		if (!file.endsWith('.json')) continue
+
+		const raw = await readFile(join(dirName, file))
 		const json: any[] = await JSON.parse(raw.toString())
 
 		count += json.length
